05efbe6a62691757f0b1473808e5ae69c7959300
Sebastián <sebaalday28@gmail.com>
2024-02-14 23:05:44 -0300

    3rd Commit: Profile section

--- website/static/js/responsive-functions.js
+++ website/static/js/responsive-functions.js
@@ -1,14 +1,17 @@
-const menu = document.getElementById("menu");
-const icon = document.getElementById("fa-caret-right");
-
-document.getElementById("toggle").addEventListener("click", function() {
+    const menu = document.getElementById("menu");
+    const toggle = document.getElementById("toggle");
     
-    if (menu.classList.contains("show-menu") || menu.classList.contains("")) {
-        menu.className = menu.className.replace("show-menu", "hide-menu");
-        icon.setAttribute("transform", "rotate(0)");
-    }
-    else if(menu.classList.contains("hide-menu")){
-        menu.className = menu.className.replace("hide-menu", "show-menu");
-        icon.setAttribute("transform", "rotate(90)");
-    }
-});
+    document.getElementById("toggle").addEventListener("click", function() {
+        
+        toggle.children[0].setAttribute("style","--fa-rotate-angle:0deg")
+
+        if (menu.classList.contains("show-menu")) {
+            menu.className = menu.className.replace("show-menu", "hide-menu");
+            toggle.children[0].setAttribute("style","--fa-rotate-angle:0deg")
+        }
+        else if(menu.classList.contains("hide-menu")){
+            menu.className = menu.className.replace("hide-menu", "show-menu");
+            toggle.children[0].setAttribute("style","--fa-rotate-angle:45deg")
+        }
+    });
+    
